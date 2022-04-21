26259787353ee4fcc62974eeef6df8e6dbf3b6f3
EtDude <Ojoexcel5@gmail.com>
2022-04-21 21:38:43 +0000

    Connection To phantom successful now

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import twitterLogo from './assets/twitter-logo.svg';
 import './App.css';
 
@@ -7,7 +7,7 @@ const TWITTER_HANDLE = 'crecia_cell';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
-  
+  const [walletAddress, setWalletAddress] = useState(null);
   const checkIfWalletIsConnected = async () => {
     try {
       const { solana } = window;
@@ -20,6 +20,7 @@ const App = () => {
             'Connected with Public Key:',
             response.publicKey.toString()
           );
+          setWalletAddress(response.publicKey.toString());
         }
       } else {
         alert('Solana object not found! Get a Phantom Wallet 👻');
@@ -30,7 +31,16 @@ const App = () => {
   };
 
  
-  const connectWallet = async () => {};
+  const connectWallet = async () => {
+  const { solana } = window;
+
+  if (solana) {
+    const response = await solana.connect();
+    console.log('Connected with Public Key:', response.publicKey.toString());
+    setWalletAddress(response.publicKey.toString());
+  }
+};
+
 
   
   const renderNotConnectedContainer = () => (
@@ -50,16 +60,18 @@ const App = () => {
     return () => window.removeEventListener('load', onLoad);
   }, []);
 
+  
+    
   return (
     <div className="App">
-      <div className="container">
+      <div className={walletAddress ? 'authed-container' : 'container'}>
         <div className="header-container">
           <p className="header">Connect your wallet</p>
           <p className="sub-text">
             View your GIF collection in the metaverse ✨
           </p>
           {}
-          {renderNotConnectedContainer()}
+          {!walletAddress && renderNotConnectedContainer()}
         </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
